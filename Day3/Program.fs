cfe40b0a29202503169d7d94a5a2d0e89ea44b5b
Daniel Edwards <daniel.edwards@esendex.com>
2019-12-03 13:44:24 +0000

    Day 3!

---
+++ Day3/Program.fs
@@ -0,0 +1,46 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+type Point = int * int 
+
+let getWirePoints () =
+    async {
+        let! wireLines = File.ReadAllLinesAsync("input.txt") |> Async.AwaitTask
+        let wirePoints = wireLines |> Array.map(fun s -> s.Split(','))
+        return wirePoints
+    }
+
+let plotPoints(previousPoint : Point, instruction : string) =
+    let amount = Int32.Parse (instruction.Substring 1)
+    let direction = instruction.[0]
+    let (previousX, previousY) = previousPoint
+    let amountArray = [1..amount] |> List.rev
+    match direction with
+        | 'U' -> List.map(fun num -> Point(previousX, previousY + (num))) amountArray
+        | 'D' -> List.map(fun num -> Point(previousX, previousY - (num))) amountArray
+        | 'L' -> List.map(fun num -> Point(previousX - (num), previousY)) amountArray
+        | 'R' -> List.map(fun num -> Point(previousX + (num), previousY)) amountArray
+        | _ -> failwith "invalid direction"
+
+let plotWire(wirePoints : string[]) = 
+    Array.fold (fun points instruction -> List.append(plotPoints (points.Head, instruction)) points) [Point(0,0)] wirePoints
+
+let findClosest(points1 : Point list, points2 : Point list) =
+    let points1Set = Set.ofList points1
+    let points2Set = Set.ofList points2
+    let orderedDistance = Set.intersect points1Set points2Set
+                            |> Set.filter(fun (x, y) -> x <> 0 && y <> 0)
+                            |> Set.map(fun (x, y) -> Math.Abs(x) + Math.Abs(y))
+                            |> Seq.sort
+                            |> List.ofSeq
+    orderedDistance.Head
+
+[<EntryPoint>]
+let main argv =
+    let wirePoints = getWirePoints() |> Async.RunSynchronously
+    let plottedWires = wirePoints |> Seq.map plotWire
+    let closestIntersection = findClosest (Seq.item(0) plottedWires, Seq.item(1) plottedWires)
+    printfn "%i" closestIntersection
+    0 // return an integer exit code
